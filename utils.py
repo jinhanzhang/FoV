39eb6e3e969b644540d9b6a6a4fb29f96e0dd5cb
Jinhan Zhang <jz5952@gv012.hpc.nyu.edu>
2024-01-18 18:55:32 -0500

    previous version without pe, bias

--- utils.py
+++ utils.py
@@ -5,10 +5,12 @@ import numpy as np
 import math
 from torch.utils.data import Dataset, DataLoader
 
+
 class FoVDataset(Dataset):
-    def __init__(self, x_data, y_data):
-        self.x_data = x_data
-        self.y_data = y_data
+    def __init__(self, x_data, y_data, feature_idx):
+        self.feature_idx = feature_idx
+        self.x_data = x_data[:,:,feature_idx]
+        self.y_data = y_data[:,:,feature_idx]
 
     def __len__(self):
         return len(self.x_data)
@@ -17,188 +19,33 @@ class FoVDataset(Dataset):
         x = torch.tensor(self.x_data[idx])
         y = torch.tensor(self.y_data[idx])
         return x,y
-
-def a_norm(Q, K, mask = None):
-    print("a_norm")
-    print("Q: ", Q.size())
-    print("K: ", K.size())
-    m = torch.matmul(Q, K.transpose(2,1).float())
-    m /= torch.sqrt(torch.tensor(Q.shape[-1]).float())
-    if mask is not None:
-        m.masked_fill(mask == 0, float("-1e20"))
-    return torch.softmax(m , -1) #(batch_size, dim_attn, seq_length)
-
-
-def attention(Q, K, V, mask = None):
-    #Attention(Q, K, V) = norm(QK)V
-    print("attention")
-    print("Q: ", Q.size())
-    print("K: ", K.size())
-    print("V: ", V.size())
     
-    a = a_norm(Q, K, mask) #(batch_size, dim_attn, seq_length)
-    
-    return  torch.matmul(a,  V) #(batch_size, seq_length, seq_length)
+# visualize data, target, and prediction
+feature_size = FEATURE_SIZE
+print(feature_size)
+x_data = x_val[::8,:,:].reshape(-1,9)
+print(x_data.shape)
+y_data = y_val[::8,:,:].reshape(-1,9)
+plt.figure()
+if feature_size<=3:
+    fig, ax = plt.subplots(1, feature_size, figsize=(feature_size*4,4))
+    for i in range(feature_size):
+        ax[i].plot(x_data[:,i])
+        ax[i].plot(y_data[:,i])
+else:
+    rows = (feature_size-1)//3+1
+    fig, ax = plt.subplots(rows, 3, figsize=(12,4*rows))
+    for i in range(rows):
+        for j in range(3):
+            if i*3+j<feature_size:
+                ax[i][j].plot(x_data[:,3*i+j])
+                ax[i][j].plot(y_data[:,3*i+j])
+fig.legend(["data","target"])
+plt.show()
 
-class AttentionBlock(torch.nn.Module):
-    def __init__(self, dim_val, dim_attn):
-        super(AttentionBlock, self).__init__()
-        self.value = Value(dim_val, dim_val)
-        self.key = Key(dim_val, dim_attn)
-        self.query = Query(dim_val, dim_attn)
-    
-    def forward(self, x, kv = None, mask = None):
-        if(kv is None):
-            #Attention with x connected to Q,K and V (For encoder)
-            output =  attention(self.query(x), self.key(x), self.value(x), mask)
-        
-        #Attention with x as Q, external vector kv as K an V (For decoder)
-        output = attention(self.query(x), self.key(kv), self.value(kv), mask)
-        
-        return output
-    
-class MultiHeadAttentionBlock(torch.nn.Module):
-    def __init__(self, dim_val, dim_attn, n_heads):
-        super(MultiHeadAttentionBlock, self).__init__()
-        self.heads = []
-        for i in range(n_heads):
-            self.heads.append(AttentionBlock(dim_val, dim_attn))
-        
-        self.heads = nn.ModuleList(self.heads)
-        
-        self.fc = nn.Linear(n_heads * dim_val, dim_val, bias = False)
-                      
-        
-    def forward(self, x, kv = None, mask = None):
-        a = []
-        for h in self.heads:
-            a.append(h(x, kv = kv, mask = mask))
-            
-        a = torch.stack(a, dim = -1) #combine heads
-        a = a.flatten(start_dim = 2) #flatten all head outputs
-        
-        x = self.fc(a)
-        
-        return x
-    
-class Value(torch.nn.Module):
-    def __init__(self, dim_input, dim_val):
-        super(Value, self).__init__()
-        self.dim_val = dim_val
-        
-        self.fc1 = nn.Linear(dim_input, dim_val, bias = False)
-        #self.fc2 = nn.Linear(5, dim_val)
-    
-    def forward(self, x):
-        x = self.fc1(x)
-        print("Value")
-        print("V: ", x.size())
-        #x = self.fc2(x)
-        
-        return x
 
-class Key(torch.nn.Module):
-    def __init__(self, dim_input, dim_attn):
-        super(Key, self).__init__()
-        self.dim_attn = dim_attn
-        
-        self.fc1 = nn.Linear(dim_input, dim_attn, bias = False)
-        #self.fc2 = nn.Linear(5, dim_attn)
-    
-    def forward(self, x):
-        x = self.fc1(x)
-        #x = self.fc2(x)
-        print("Key")
-        print("K: ", K.size())
-        
-        return x
 
-class Query(torch.nn.Module):
-    def __init__(self, dim_input, dim_attn):
-        super(Query, self).__init__()
-        self.dim_attn = dim_attn
-        
-        self.fc1 = nn.Linear(dim_input, dim_attn, bias = False)
-        #self.fc2 = nn.Linear(5, dim_attn)
-    
-    def forward(self, x):
-        
-        x = self.fc1(x)
-        #print(x.shape)
-        #x = self.fc2(x)
-        print("Query")
-        print("Q: ", Q.size())
-        
-        return x
 
-# https://pytorch.org/tutorials/beginner/transformer_tutorial.html
-class PositionalEncoding(nn.Module):
-    def __init__(self, d_model, dropout=0.1, max_len=600):
-        super(PositionalEncoding, self).__init__()
-
-#         pe = torch.zeros(max_len,1, d_model)
-#         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        
-#         div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
-#         print(div_term.shape)
-#         print(div_term)
-#         print(pe[:, 0, 0::2].shape)
-#         print(pe[:, 0, 1::2].shape)
-#         print(torch.sin(position * div_term).shape)
-#         print(torch.cos(position * div_term).shape)
-        
-#         pe[:, 0::2] = torch.sin(position * div_term)
-#         pe[:, 1::2] = torch.cos(position * div_term)
-        
-#         pe = pe.unsqueeze(0).transpose(0, 1)
-        
-#         self.register_buffer('pe', pe)
-        position = torch.arange(max_len).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
-        pe = torch.zeros(max_len, 1, d_model)
-        print(div_term.shape)
-        print(div_term)
-        print(pe.shape)
-        print(pe[:, 0, 0::2].shape)
-        print(pe[:, 0, 1::2].shape)
-        print(torch.sin(position * div_term).shape)
-        print(torch.cos(position * div_term).shape)
-        pe[:, 0, 0::2] = torch.sin(position * div_term)
-        pe[:, 0, 1::2] = torch.cos(position * div_term)
-        self.register_buffer('pe', pe)
-
-    def forward(self, x):
-#         x = x + self.pe[:x.size(1), :].squeeze(1)
-#         torch.permute(pe, (1, 0, 2))
-        torch.permute(x, (1, 0, 2))
-        x = x + self.pe[:x.size(0)]
-        torch.permute(x, (1, 0, 2))
-        return x  
-
-#TODO
-class RelativePositionalEncoding(nn.Module):
-    def __init__(self, d_model, dropout=0.1, max_len=5000):
-        super(PositionalEncoding, self).__init__()
-
-        pe = torch.zeros(max_len, d_model)
-        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        
-        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
-        print(div_term.shape)
-        print(pe[:, 1::2].shape)
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
-        print(div_term.shape)
-        print(pepe[:, 1::2].shape)
-        
-        
-        pe = pe.unsqueeze(0).transpose(0, 1)
-        
-        self.register_buffer('pe', pe)
-
-    def forward(self, x):
-        x = x + self.pe[:x.size(1), :]. squeeze(1)
-        return x 
     
 def get_data(batch_size, input_sequence_length, output_sequence_length):
     i = input_sequence_length + output_sequence_length
@@ -209,27 +56,3 @@ def get_data(batch_size, input_sequence_length, output_sequence_length):
     s = torch.sigmoid(b.float())
     return s[:, :input_sequence_length].unsqueeze(-1), s[:,-output_sequence_length:]
 
-# checkpointing
-def save_ckpt(path, model, optimizer, epoch, val_loss):
-    
-#     torch.save({'net': network.state_dict(), 'opt': optimizer.state_dict()}, fn)
-    path = path + '/epoch_' + str(epoch) + '_ckpts.pt'
-    torch.save({
-            'epoch': epoch,
-            'model_state_dict': model.state_dict(),
-            'optimizer_state_dict': optimizer.state_dict(),
-            'val_loss': loss,
-            }, path)
-
-def load_ckpt(path, model, optimizer, device='cuda'):
-    if device == 'cpu':
-        checkpoint = torch.load(path,map_location=torch.device('cpu'))
-    else:
-        checkpoint = torch.load(path)
-    model.load_state_dict(checkpoint['model_state_dict'])
-    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-    epoch = checkpoint['epoch']
-    val_loss = checkpoint['val_loss']
-
-    model.eval()
-    return model, optimizer
