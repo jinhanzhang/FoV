6617ef69a48d07145d7b8a1c4290a42d82373c9d
james20141606 <240682348@qq.com>
2024-02-25 19:28:56 -0500

    update

--- sync_to_local.sh
+++ sync_to_local.sh
@@ -1,1 +1,1 @@
-rsync -avzh --exclude={'checkpoints/','checkpoints','processed_data/','processed_data','saved_results','models','time_series_lib'} greene:/scratch/xc1490/projects/FoV/* .
+rsync -avzh --exclude={'checkpoints/','checkpoints','processed_data/','processed_data','saved_results','models','examples','time_series_lib'} greene:/scratch/xc1490/projects/FoV/* .
